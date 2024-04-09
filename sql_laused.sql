63d2aeb2d5e129bdec93465b6a56757d3dfcc82a
Dmitri Trostnikov <trostnikov06@mail.ru>
2024-04-09 15:07:31 +0300

    sql

--- sql_laused.sql
+++ sql_laused.sql
@@ -22,3 +22,36 @@ nimetus, kestvus, algus_kuupaev, kirjeldus)
 VALUES (
 'Eesti Keel', 200, '2024-04-9', 'Üldainete moodul');
 SELECT * FROM oppeaine;
+
+--tabeli kustutamine
+DROP table oppeaine;
+
+--Tabel Opetaja
+CREATE TABLE opetaja(
+opetajaID int Primary key identity(1,1),
+eesnimi varchar(50),
+perenimi varchar(50) NOT NULL,
+synniaeg date,
+kontakt varchar(50));
+SELECT * from opetaja;
+INSERT INTO opetaja(eesnimi, perenimi, kontakt)
+VALUES ('Mihail', 'Agapov', '76543578');
+
+--andmete uuendamine
+UPDATE opetaja SET synniaeg='3000-10-10'
+WHERE opetajaID=1;
+--tabeli muutmine veergu lisamine
+--FK--->PK
+ALTER TABLE oppeaine ADD opetajaID int;
+SELECT * FROM oppeaine;
+SELECT * FROM opetaja;
+UPDATE oppeaine SET opetajaID=1;
+--Foreign key lisamine tabeli sisse
+ALTER TABLE oppeaine
+ADD FOREIGN KEY (opetajaID) REFERENCES opetaja(opetajaID);
+
+INSERT INTO oppeaine(
+nimetus, kestvus, algus_kuupaev, kirjeldus, opetajaID)
+VALUES (
+'Linux', 600, '2024-04-9', 'Linux moodul moodul', 1);
+SELECT * FROM oppeaine;
